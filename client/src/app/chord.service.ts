ef564047ef93f212938d574f9a716fa3085fd6f1
Evan Cole <ecole5@uwo.ca>
2016-11-24 23:30:47 -0500

    Almost done error parser for chord

--- client/src/app/chord.service.ts
+++ client/src/app/chord.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map'; //why do we import this here yet use it in the service
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ChordService {
 private chordsUrl = 'api/chord';
 constructor (private http: Http){}
+
 getChords(){
     return this.http.get(this.chordsUrl).map(res=> res.json());
 
 }
+
+
+
+
+
+
+
 
 
 
--- client/src/app/edit-chord/edit-chord.component.ts
+++ client/src/app/edit-chord/edit-chord.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class EditChordComponent{
 
+  errors = [];
   clearSelected = '';
   clear(){
     if (this.clearSelected == ''){
@@ -17,4 +18,131 @@ export class EditChordComponent{
     }
        
   }
+
+validateChordPro(text){
+  //Break chordpro into lines, remove whitespace
+  var lines = text.split('\n'); 
+  for (let i = 0; i < lines.length; i++){
+    lines[i] = lines[i].trim();
+  }
+  
+  //control varaibles
+  this.errors = [];
+  let titleFound = 0;
+  let startSong = 0;
+
+  for (let i = 0; i< lines.length; i++){
+    let currentLine = lines[i];
+    if (currentLine[0] == "#"){continue;} //ignore comment line
+    
+    //deal with possible directive line
+    if (currentLine.indexOf("{") > -1 || currentLine.indexOf("}") > -1){
+      let tempa = currentLine.length - currentLine.replace("{", "").length;
+      let tempb = currentLine.length- currentLine.replace("}", "").length;
+
+      if (tempa == 1 && tempb == 1){
+      
+        if (currentLine == "{ns}" || currentLine == "{new_song}"){
+           startSong++;
+           if (startSong > 1) {
+                this.errors.push("Warning: More than one new song is not alowed. See line: " + i.toString()); 
+              
+           }
+
+        }
+        else if (currentLine.indexOf("t") == 1 || currentLine.indexOf("title") == 1 ){
+    
+                  if (currentLine.indexOf(":") == -1 || currentLine[currentLine.indexOf(":") + 1] == "}"){
+                     this.errors.push("Error: Missing seperator or text after sepeator or misspled directive: " + i.toString());
+            }
+            titleFound++;
+            if (titleFound > 1){
+               this.errors.push("Error: More than one title directive. See line: " + i.toString()); 
+
+            }
+
+        }
+        else if (currentLine.indexOf("st") == 1 || currentLine.indexOf("subtitle") == 1 ){
+                 if (currentLine.indexOf(":") == -1 || currentLine[currentLine.indexOf(":") + 1] == "}"){
+              this.errors.push("Error: Missing seperator or text after sepeator or misspled directive: " + i.toString());
+            }
+
+        }
+        else if (currentLine.indexOf("c") == 1 || currentLine.indexOf("comment") == 1 ){
+                if (currentLine.indexOf(":") == -1 || currentLine[currentLine.indexOf(":") + 1] == "}"){
+               this.errors.push("Error: Missing seperator or text after sepeator or misspled directive: " + i.toString());
+            }
+
+        }
+        else if (currentLine.indexOf("define") == 1){
+             if (currentLine.indexOf(":") == -1 || currentLine[currentLine.indexOf(":") + 1] == "}"){
+              this.errors.push("Error: Missing seperator or text after sepeator or misspled directive: " + i.toString()); 
+            }
+            //Check more stuff here
+        }
+        else if (currentLine.indexOf("soc") > -1 || currentLine.indexOf("start_of_chorus") > -1){
+          if (currentLine != "{soc}" || currentLine != "{start_of_chorus}"){
+            this.errors.push("Error: The directive has some extra characters : " + i.toString()); 
+          }
+             
+        }
+         else if (currentLine.indexOf("eoc") > -1 || currentLine.indexOf("end_of_chorus") > -1){
+          if (currentLine != "{eoc}" || currentLine != "{end_of_chorus}"){
+            this.errors.push("Error: The directive has some extra characters : " + i.toString()); 
+          }
+             
+        }
+        else if (currentLine.indexOf("eot") > -1 || currentLine.indexOf("end_of_tab") > -1){
+          if (currentLine != "{eot}" || currentLine != "{end_of_tab}"){
+            this.errors.push("Error: The directive has some extra characters : " + i.toString()); 
+          }
+             
+        }
+         else if (currentLine.indexOf("sot") > -1 || currentLine.indexOf("start_of_tab") > -1){
+          if (currentLine != "{sot}" || currentLine != "{start_of_tab}"){
+            this.errors.push("Error: The directive has some extra characters : " + i.toString()); 
+          }
+             
+        }
+        
+       
+        else{
+             this.errors.push("Warning: Undefined directive on line: " + i.toString()); 
+        }
+        
+      
+      
+      } //end of individual directive problems
+      else {
+        this.errors.push("Error: Brace Mismatch on line: " + i.toString()); 
+      }
+    }//end of possible directive detection
+//normal line operation
+
+
+else{
+  if (titleFound == 0 || startSong == 0){
+     this.errors.push("Error: You must have both a ns and t directive before starting lyrics. See line: " + i.toString());
+
+
+  }
+}
+
+
+
+  }//end of for loop
+
+//case where no content
+if (titleFound == 0 || startSong == 0){
+      this.errors.push("Error: You must have both a ns and t directive." + i.toString());
+}
+} //end of function
+
+
+  submit(text){
+    this.validateChordPro(text);
+
+  } 
+
+
 }
